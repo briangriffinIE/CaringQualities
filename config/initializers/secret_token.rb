57c472af32457da42b7e8e85569f0b5af5169c60
Brian Griffin <brian.griffin@visitorM.com>
2015-05-21 21:55:31 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaringQualities::Application.config.secret_key_base = '94a8948308879e7710eafeb2856fe42b365e226d28f6296d6c146ea528b8c4eeaf167647d280484a0b199ee2daf4ce1f6c433c1f553f484a67012b8bcd328756'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CaringQualities::Application.config.session_store :cookie_store, key: '_caringQualities_session'
